30d68f1ca866d6e82a975f4a5e41e5ca3b46fb68
technocode <madeinborneo@gmail.com>
2025-12-30 20:28:48 +0800

    Add favicon support across multiple views and enhance settings.local.json with additional permissions

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Privacy Policy - {{ config('app.name') }}</title>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="alternate icon" href="{{ asset('favicon.ico') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="antialiased bg-white">
